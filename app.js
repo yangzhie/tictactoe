a2c207b1afa4ad4a01218c9bc8f3246f8bd0934f
ArnavMalasi <arnavmalasi2212@gmail.com>
2024-04-21 01:05:46 +1000

    change to Sonic theme. index.html & CSS updated accordingly - revamp. app.js old code keep, add other functions in future to make functional.

--- app.js
+++ app.js
@@ -1,139 +1,188 @@
-//== game state (globals)
-
-let currentPlayer = 1;
-let p1Wins = 0;
-let p2Wins = 0;
-let draws = 0;
-let isGameActive = true;
-const board = ["", "", "", "", "", "", "", "", ""];
-const winCombos = [
-    [0, 1, 2],
-    [3, 4, 5],
-    [6, 7, 8],
-    [0, 3, 6],
-    [1, 4, 7],
-    [2, 5, 8],
-    [0, 4, 8],
-    [2, 4, 6]
-];
-
-
-//== caching DOM element references
-
-const boxes = document.querySelectorAll(".box");
-const turnStatus = document.querySelector(".turn-status");
-const p1WinsDisplay = document.querySelector(".p1-wins");
-const p2WinsDisplay = document.querySelector(".p2-wins");
-const drawsDisplay = document.querySelector(".draws");
-const restartBtn = document.querySelector(".restart-button");
-
-//== set up event listeners
-
-function handleBoxClickEvent(box) {
-    box.addEventListener('click', handleBoxClick);
+const playerOne = {
+    name: '',
+    avatar: '',
+    hearts: 0,
+    choicesArr: [],
+    matchesArr: [],
+    winOrLoss: []
 }
 
-for (let i = 0; i < boxes.length; i++) {
-    handleBoxClickEvent(boxes[i]);
+const playerTwo = {
+    name: null,
+    avatar: null,
+    hearts: 0,
+    choicesArr: [],
+    matchesArr: [],
+    winOrLoss: []
 }
 
-restartBtn.addEventListener("click", resetGame);
+let currentPlayer = null
+let roundCount = 1
 
-//== event handlers
+// player details
+const playerOneNameElem = document.querySelector('.player1 input')
+const playerTwoNameElem = document.querySelector('.player2 input')
+const playerOneAvatarElem = document.querySelector('.player1-avatar')
+const playerTwoAvatarElem = document.querySelector('.player2-avatar')
+const playerOneHpElems = document.querySelectorAll('.player1 .active-hearts')
+const playerTwoHpElems = document.querySelectorAll('.player2 .active-hearts')
 
-// checkWin function to determine with every press of a square if
-// game ends on a draw, win or keeps going
-function checkWin() {
-    for (let i = 0; i < winCombos.length; i++) {
-        const [x, y, z] = winCombos[i];
-        if (board[x] !== "" && board[x] === board[y] && board[x] === board[z]) {
-            isGameActive = false;
-            return board[x];
-        }
-    }
+// setup details
+const messageWrapperElem = document.querySelector('.message-wrapper')
+const avatarWrapperElem = document.querySelector('.avatar-wrapper')
+const avatarChoicesElems = document.querySelectorAll('.avatar-choices')
+const messageElem = document.querySelector('.message')
+const playerOneTurnElem = document.querySelector('#player1-turn')
+const playerTwoTurnElem = document.querySelector('#player2-turn')
+const nextRoundElem = document.querySelector('.next-round-btn')
+const resetElem = document.querySelector('.reset-btn')
+const boxElems = document.querySelectorAll('.board div')
+const boardElem = document.querySelector('.board')
+const roundCountElem = document.querySelector('#round-count')
+const backgroundMusicElem = document.querySelector('#background-music')
+const playMusicElem = document.querySelector('#play-music')
+const muteMusicElem = document.querySelector('#mute-music')
 
-    // game keeps going if any one of the squares on the board
-    // is empty. i.e square has not yet been clicked by player
-    let isADraw = true;
-    for (let i = 0; i < board.length; i++) {
-        if (board[i] === "") {
-            isADraw = false;
-            break;
-        }
-    }
+// event listeners
+for (let avatarChoiceElem of avatarChoicesElems) {
+    avatarChoiceElem.addEventListener('click', handleSetPlayerAvatar)
+}
+playerOneNameElem.addEventListener('blur', isPlayerOneNameFilled)
+playerTwoNameElem.addEventListener('blur', isPlayerTwoNameFilled)
+playerOneTurnElem.addEventListener('click', handlePickFirstTurn)
+playerTwoTurnElem.addEventListener('click', handlePickFirstTurn)
+nextRoundElem.addEventListener('click', handleNextRound)
+resetElem.addEventListener('click', handleReset)
+playerOneNameElem.addEventListener('focus', handleBackgroundMusic)
+playMusicElem.addEventListener('click', handleBackgroundMusic)
+muteMusicElem.addEventListener('click', handleMuteMusic)
 
-    // else game ends in a draw
-    if (isADraw) {
-        isGameActive = false;
-        return "draw";
-    }
-
-    return null;
+function handleMuteMusic() {
+    backgroundMusicElem.muted = true
 }
 
-function handleBoxClick(event) {
-    const selectedBox = event.target;
-    const boxIndex = Number(selectedBox.dataset.box) - 1;
-
-    if (board[boxIndex] === "" && isGameActive) {
-        selectedBox.textContent = currentPlayer === 1 ? "X" : "O"; // condensed if... else statement
-        board[boxIndex] = currentPlayer === 1 ? "X" : "O"; // condensed if... else statement
-
-        const result = checkWin();
-        // player 1 wins
-        if (result === "X") {
-            p1Wins++;
-            p1WinsDisplay.textContent = p1Wins;
-            turnStatus.textContent = "Player 1 wins!";
-
-            // player 2 wins
-        } else if (result === "O") {
-            p2Wins++;
-            p2WinsDisplay.textContent = p2Wins;
-            turnStatus.textContent = "Player 2 wins!";
-
-            // draw
-        } else if (result === "draw") {
-            draws++;
-            drawsDisplay.textContent = draws;
-            turnStatus.textContent = "The game has ended in a draw!";
-
-            // game keeps going
-        } else {
-            currentPlayer = currentPlayer === 1 ? 2 : 1;
-            turnStatus.textContent = `Player ${currentPlayer}'s Turn`;
-        }
-    }
-}
-
-// reset game function to original state
-// does not modify pre-existing wins and draws
-function resetGame() {
-    for (let i = 0; i < board.length; i++) {
-        board[i] = "";
-        boxes[i].textContent = "";
-    }
-
-    currentPlayer = 1;
-    isGameActive = true;
-    turnStatus.textContent = "Player 1's Turn";
+function handleBackgroundMusic() {
+    backgroundMusicElem.play()
+    backgroundMusicElem.muted = false
+    backgroundMusicElem.volume = 0.3
 }
 
 
+function isPlayerOneNameFilled() {
+    if (!playerOneNameElem.value) {
+        return
+    }
+    playerOne.name = playerOneNameElem.value
+    toggleAvatarPointerEvent(avatarWrapperElem)
+    playerOneNameElem.setAttribute('disabled', true)
+    messageElem.textContent = 'Player 1 choose your avatar'
+}
 
+function isPlayerTwoNameFilled() {
+    if (!playerTwoNameElem.value) {
+        return
+    }
+    playerTwo.name = playerTwoNameElem.value
+    toggleAvatarPointerEvent(avatarWrapperElem)
+    playerTwoNameElem.setAttribute('disabled', true)
+    messageElem.textContent = 'Player 2 choose your avatar'
+}
 
+function handleSetPlayerAvatar(event) {
+    if (!playerOne.avatar) {
+        playerOne.avatar = event.target.src
+        playerOneAvatarElem.src = playerOne.avatar
+        playerOneAvatarElem.style.display = 'block'
+        playerTwoNameElem.removeAttribute('disabled')
+        toggleAvatarPointerEvent(avatarWrapperElem)
+        messageElem.textContent = 'Player 2 enter your name below'
+    } else {
+        playerTwo.avatar = event.target.src
+        toggleAvatarPointerEvent(avatarWrapperElem)
+        playerTwoAvatarElem.src = playerTwo.avatar
+        playerTwoAvatarElem.style.display = 'block'
+        playerOneTurnElem.src = playerOne.avatar
+        playerTwoTurnElem.src = playerTwo.avatar
+        avatarWrapperElem.style.display = 'none'
+        playerOneTurnElem.style.display = 'inline'
+        playerTwoTurnElem.style.display = 'inline'
+        messageElem.textContent = 'Who will go first?'
+        handlePickFirstTurn
+    }
+    event.target.style.display = 'none'
+}
 
+function handlePickFirstTurn(event) {
 
+    if (event.target.src === playerOne.avatar) {
+        currentPlayer = playerOne
+    } else {
+        currentPlayer = playerTwo
+    }
+    if (event.target.src !== playerOneTurnElem.src) {
+        playerOneTurnElem.style.display = 'none'
+    } else {
+        playerTwoTurnElem.style.display = 'none'
+    }
+    messageElem.textContent = `${currentPlayer.name} will go first`
+    setTimeout(startGame, 2000)
+}
 
+function handlePlayerChoice(event) {
+    let choiceElem = event.target
+    choiceElem.style.pointerEvents = 'none'
+    choiceElem.style.backgroundImage = `url(${currentPlayer.avatar})`
+    let numberChoice = Number(choiceElem.dataset.num)
+    currentPlayer.choicesArr.push(numberChoice)
+    if (currentPlayer.choicesArr.length >= 3) {
+        checkForWinningPattern(currentPlayer)
+        decideWinOrDraw(playerOne, playerTwo)
+    }
+    switchPlayer()
+}
 
+function handleNextRound() {
+    roundCount++
+    roundCountElem.textContent = roundCount
+    toggleMessageElemDisplay()
+    messageElem.textContent = ''
+    resetBoxElems()
+    resetPlayersChoices()
+    resetPlayersMatchesArr()
+    for (let boxElem of boxElems) {
+        boxElem.style.pointerEvents = 'auto'
+    }
+    handlePlayerChoice
+}
 
+function handleReset() {
 
+    resetElem.style.display = 'none'
+    messageElem.textContent = ''
+    playerOne.name = ''
+    playerTwo.name = ''
+    playerOne.avatar = ''
+    playerTwo.avatar = ''
+    avatarWrapperElem.style.display = 'block'
+    playerOneHpElems.forEach(elem => elem.style.display = 'inline')
+    playerTwoHpElems.forEach(elem => elem.style.display = 'inline')
+    resetPlayersChoices()
+    resetPlayersMatchesArr()
+    resetBoxElems()
+    roundCount = 1
+    roundCountElem.textContent = roundCount
+    playerOneAvatarElem.setAttribute('src', '')
+    playerTwoAvatarElem.setAttribute('src', '')
+    playerOneTurnElem.src = ''
+    playerTwoTurnElem.src = ''
+    playerOneNameElem.removeAttribute('disabled')
+    playerTwoNameElem.removeAttribute('disabled')
+    playerOneNameElem.value = null
+    playerTwoNameElem.value = null
+    avatarChoicesElems.forEach(elem => elem.style.display = 'inline')
+    messageElem.textContent = 'Player 1 enter your name below'
+    playerTwoNameElem.setAttribute('disabled', true)
+    toggleAvatarPointerEvent()
 
-
-
-
-
-
-
-
-
+    isPlayerOneNameFilled()
+}
